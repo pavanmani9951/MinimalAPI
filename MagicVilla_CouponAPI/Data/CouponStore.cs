f1213dad01e7e89057467207d5f4289c10c0c175
pavan <mani365@gmail.com>
2024-01-30 16:23:43 +0530

    created separate file for data and models

---
+++ MagicVilla_CouponAPI/Data/CouponStore.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_CouponAPI.Models;
+
+namespace MagicVilla_CouponAPI.Data
+{
+    public static class CouponStore
+    {
+        public static List<Coupon> Coupons = new List<Coupon>{
+            new Coupon() {Id=1,Name="Myntra",Percent=30,IsActive=false },
+            new Coupon() {Id=2,Name="Flipkart",Percent=45,IsActive=false },
+            new Coupon() {Id=3,Name="Ajio",Percent=65,IsActive=false },
+            new Coupon() {Id=4,Name="Amazon",Percent=20,IsActive=false }
+        };
+    }
+}   
